5b4df32b981575c035af3bd5b4370051b54fd08a
VReaperV <reaper9977@gmail.com>
2025-03-30 23:05:58 +0300

    Move GetAverageLuminance() to the correct ifdef

--- src/engine/renderer/glsl_source/cameraEffects_fp.glsl
+++ src/engine/renderer/glsl_source/cameraEffects_fp.glsl
@@ -79,11 +79,12 @@ vec3 TonemapLottes( vec3 color ) {
 	layout(std430, binding = BIND_LUMINANCE_STORAGE) readonly buffer luminanceBuffer {
 		uint luminanceU;
 	};
+
+	float GetAverageLuminance( const in uint luminance ) {
+		return float( luminanceU ) / ( u_TonemapParms2[1] * u_ViewWidth * u_ViewHeight );
+	}
 #endif
 
-float GetAverageLuminance( const in uint luminance ) {
-    return float( luminanceU ) / ( u_TonemapParms2[1] * u_ViewWidth * u_ViewHeight );
-}
 #endif
 
 DECLARE_OUTPUT(vec4)
