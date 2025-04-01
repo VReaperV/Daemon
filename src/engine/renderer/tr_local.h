7ff8bc6d28c7bcc55a4342c6232e6c2ebdae7f36
VReaperV <reaper9977@gmail.com>
2025-04-01 15:18:26 +0300

    Move duplicate code from ParseFace() and ParseTriSurf() into ParseTriangleSurface()
    Also moves `plane` from `srfSurfaceFace_t` to `srfSurfaceGeneric_t`, since it's the only difference between them, and plane computations were already being done on `srfSurfaceGeneric_t` anyway, so they can be moved there too.

--- src/engine/renderer/tr_local.h
+++ src/engine/renderer/tr_local.h
@@ -1739,6 +1739,8 @@ enum class ssaoMode {
 		vec3_t origin;
 		float radius;
 
+		cplane_t plane;
+
 		int numVerts;
 		srfVert_t* verts;
 
@@ -1769,7 +1771,6 @@ enum class ssaoMode {
 	};
 
 	struct srfSurfaceFace_t : srfGeneric_t {
-		cplane_t plane;
 	};
 
 	struct srfVBOMesh_t : srfGeneric_t {
