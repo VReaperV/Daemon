53f636f42e2dbc496177b1a2a4abc85e404abd20
VReaperV <reaper9977@gmail.com>
2025-06-16 23:58:49 +0300

    .

--- src/engine/renderer/tr_local.h
+++ src/engine/renderer/tr_local.h
@@ -2239,8 +2239,7 @@ enum class ssaoMode {
 		float           vertexAttribsInterpolation; // 0 = no interpolation, 1 = final position
 		uint32_t        vertexAttribsNewFrame; // offset for VBO vertex animations
 		uint32_t        vertexAttribsOldFrame; // offset for VBO vertex animations
-		ShaderProgramDescriptor* currentProgram;
-		ShaderPipelineDescriptor* currentPipeline;
+		GLuint currentPipeline;
 		FBO_t           *currentFBO;
 		VBO_t           *currentVBO;
 		IBO_t           *currentIBO;
@@ -2992,10 +2991,8 @@ inline bool checkGLErrors()
 	void GL_Unbind( image_t *image );
 	GLuint64 BindAnimatedImage( int unit, const textureBundle_t *bundle );
 	GLuint64 GL_BindToTMU( int unit, image_t* image );
-	void GL_BindProgram( ShaderProgramDescriptor* program );
-	void GL_BindNullProgram();
-	void GL_BindProgramPipeline( ShaderPipelineDescriptor* program );
-	void GL_BindNullProgramPipeline();
+	void GL_BindProgram( GLuint pipeline, bool override = false );
+	void GL_BindNullProgram( bool override = false );
 	void GL_SetDefaultState();
 	void GL_SelectTexture( int unit );
 	void GL_TextureMode( const char *string );
